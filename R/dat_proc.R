35d278ec70a5080e6b57d5710d19e157edf40596
fawda123 <mbafs2012@gmail.com>
2020-03-12 16:57:33 -0400

    working hindcast model

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -3,15 +3,44 @@ library(R.matlab)
 library(here)
 library(lubridate)
 
-# model parameters --------------------------------------------------------
 
-modprm <- readMat(here::here('data/data-raw/LR_final_20200109.mat'))
+# recreate glm ------------------------------------------------------------
 
-save(modprm, file = here::here('data/modprm.RData'), compress = 'xz')
+# import data
+indvar0 <- read.csv(here::here('data/data-raw/LR_final_indvar0.csv'), stringsAsFactors = F)
+depvar0 <- read.csv(here::here('data/data-raw/LR_final_depvar0.csv'), stringsAsFactors = F)
 
-# model data
+# combine independent, dependent, rename some things
+dat <- bind_cols(depvar0, indvar0) %>% 
+  select(
+    overflow = DepVar, 
+    precp = `indvar0...1.`,
+    var2 = `indvar0...2.`,
+    water = `indvar0...3.`, 
+    var4 = `indvar0...4.`, 
+    date = stime0
+  ) %>% 
+  mutate(date = dmy(date))
+
+# remove all records where water < mean water level
+dat <- dat %>% 
+  filter(water >= mean(water))
+
+# model
+rskmod <- glm(overflow ~ precp + water + var2 + var4, family =binomial(link = "logit"), data = dat)
+
+save(rskmod, file = here::here('data/rskmod.RData'), compress = 'xz')
+
+# model data --------------------------------------------------------------
 
 indvar0 <- read.csv(here::here('data/data-raw/LR_final_indvar0.csv'), stringsAsFactors = F) %>% 
-  mutate(stime0 = dmy(stime0))
+  mutate(stime0 = dmy(stime0)) %>% 
+  rename(
+    precp = `indvar0...1.`,
+    var2 = `indvar0...2.`,
+    water = `indvar0...3.`, 
+    var4 = `indvar0...4.`, 
+    date = stime0
+  )
 
 save(indvar0, file = here::here('data/indvar0.RData'), compress = 'xz')
--- R/funcs.R
+++ R/funcs.R
@@ -1,6 +1,6 @@
 # probability of event
-hindcastSSO <- function(t0, t1, dw, dp, indvar0, modprm){
-  
+hindcastSSO <- function(t0, t1, dw, dp, indvar0, modin){
+
   ##
   # sanity checks
   if(dw > 0.5)
@@ -9,35 +9,32 @@ hindcastSSO <- function(t0, t1, dw, dp, indvar0, modprm){
     stop('2-d precipitation too high, dp must be < 1.8 cm')
 
   # get mean, sd of water level, precip for training data
-  mnw <- mean(indvar0[, 3], na.rm = T)
-  mnp <- mean(indvar0[, 1], na.rm = T)
-  sdw <- sd(indvar0[, 3], na.rm = T)
-  sdp <- sd(indvar0[, 1], na.rm = T)
+  mnw <- mean(indvar0$water, na.rm = T)
+  mnp <- mean(indvar0$precp, na.rm = T)
+  sdw <- sd(indvar0$water, na.rm = T)
+  sdp <- sd(indvar0$precp, na.rm = T)
   
-
+  # input data for predictions
   indvar <- indvar0
-  indvar[, 1] <- indvar[, 1] + dp
-  indvar[, 3] <- indvar[, 3] + dw
+  indvar$precp <- indvar$precp + dp
+  indvar$water <- indvar$water + dw
 
   # subset indvar by input date range
-  ok <- with(indvar, stime0 >= t0 & stime0 < t1)
+  ok <- with(indvar, date >= t0 & date< t1)
   indvar <- indvar[ok, ]
-  time <- indvar$stime0[ok]
+  time <- indvar$date[ok]
 
   # find which water levels are below the mean
   w2 <- indvar[, 3]
   low <- w2 < mnw
 
-  #------------------------------------------------
-  # execute regression to calculate p
-  # probability of non-event =(1-p)
-  #------------------------------------------------
-
-  coeff <- modprm$coeff
-  stats <- modprm$stats
+  # get predictions from model, assign prob of zero if water level below mean
+  prds <- predict(rskmod, newdata = indvar, type = 'resp')
+  prds[low] <- 0
   
-  # [pihat,dlow,dhi] = mnrval(coeff,indvar,stats);
-  # p = pihat(:,2);  #probability of event
-  # p(low) = 0;
+  # output
+  indvar$risk <- prds
+  
+  return(indvar)
 
 }
